4f13bbbccf3e686fda93dfc0d655cf1bb01183f1
Fuc le <quangphuchoctapit@gmail.com>
2023-12-09 15:58:46 +0700

    fix CORS error

--- src/server.js
+++ src/server.js
@@ -7,6 +7,17 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 8081
 
+//add headers before the routes are defined
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL)
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+})
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
